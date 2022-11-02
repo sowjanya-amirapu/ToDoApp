7e84fedbec51657e1efb348073609d60d89f5be2
Sowjanya Amirapu <sowjanyaamirapu@Sowjanyas-MBP.lan>
2022-11-02 16:28:11 -0500

    Adding new files to repo :wq

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import TaskList from "./TaskList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      task: "",
+      taskArray: [],
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ task: event.target.value });
+    console.log(this.state.task);
+  };
+
+  handleAddTask = () => {
+    console.log("task set to empty");
+    this.state.taskArray.push(this.state.task);
+    this.setState({ task: "" });
+
+    console.log(this.state.taskArray, "taskArray");
+
+    //empty the text field
+    //add the task below
+
+    // const myTasks = this.state.taskArray
+
+    // myTasks.map((task) => {
+
+    // })
+  };
+
+  render() {
+    console.log(this.state.task);
+    return (
+      <>
+        <h1>ToDo List</h1>
+        <fieldset>
+          <div className="input-group flex-nowrap">
+            <span className="input-group-text" id="addon-wrapping">
+              TASK
+            </span>
+            <input
+              value={this.state.task}
+              onChange={this.handleChange}
+              type="text"
+              className="form-control"
+              placeholder="New Task"
+              aria-label="Username"
+              aria-describedby="addon-wrapping"
+            />
+          </div>
+          <button onClick={this.handleAddTask} className="btn btn-info m-2">
+            Add Task
+          </button>
+        </fieldset>
+
+        <button className="btn btn-info m-2">All List</button>
+        <button className="btn btn-info m-2">Done List</button>
+        <button className="btn btn-info m-2">ToDo List</button>
+        {/* <TaskList
+        task = {this.state.task}
+        
+      /> */}
+        <ul>
+          {this.state.taskArray.map((myTask) => {
+            return <TaskList task ={myTask} />;
+          })}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
